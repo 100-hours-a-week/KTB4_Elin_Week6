8b5962e75eb2898380e59f562a3864442e1c3c98
kcr2120 <kcr2120@gmail.com>
2026-06-22 02:26:55 +0900

    Delete api directory

--- api/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package community.api.repository;
-
-import community.api.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
